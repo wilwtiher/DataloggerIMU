924bbfc267f1c04dbae237b16841ece245397533
wilwtiher <wiltonjrtablet@gmail.com>
2025-07-28 19:35:51 -0300

    clone codigo Wilton IMU

--- DataloggerIMU.c
+++ DataloggerIMU.c
@@ -1,14 +1,502 @@
+#include "pico/binary_info.h"
+#include "hardware/i2c.h"
+#include "ssd1306.h"
+#include "font.h"
+
+#include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "hardware/adc.h"
+#include "hardware/rtc.h"
 #include "pico/stdlib.h"
 
+#include "ff.h"
+#include "diskio.h"
+#include "f_util.h"
+#include "hw_config.h"
+#include "my_debug.h"
+#include "rtc.h"
+#include "sd_card.h"
 
+#define ADC_PIN 26 // GPIO 26
+
+static bool logger_enabled;
+static const uint32_t period = 1000;
+static absolute_time_t next_log_time;
+
+static char filename[20] = "adc_data1.txt";
+
+static sd_card_t *sd_get_by_name(const char *const name)
+{
+    for (size_t i = 0; i < sd_get_num(); ++i)
+        if (0 == strcmp(sd_get_by_num(i)->pcName, name))
+            return sd_get_by_num(i);
+    DBG_PRINTF("%s: unknown name %s\n", __func__, name);
+    return NULL;
+}
+static FATFS *sd_get_fs_by_name(const char *name)
+{
+    for (size_t i = 0; i < sd_get_num(); ++i)
+        if (0 == strcmp(sd_get_by_num(i)->pcName, name))
+            return &sd_get_by_num(i)->fatfs;
+    DBG_PRINTF("%s: unknown name %s\n", __func__, name);
+    return NULL;
+}
+
+static void run_setrtc()
+{
+    const char *dateStr = strtok(NULL, " ");
+    if (!dateStr)
+    {
+        printf("Missing argument\n");
+        return;
+    }
+    int date = atoi(dateStr);
+
+    const char *monthStr = strtok(NULL, " ");
+    if (!monthStr)
+    {
+        printf("Missing argument\n");
+        return;
+    }
+    int month = atoi(monthStr);
+
+    const char *yearStr = strtok(NULL, " ");
+    if (!yearStr)
+    {
+        printf("Missing argument\n");
+        return;
+    }
+    int year = atoi(yearStr) + 2000;
+
+    const char *hourStr = strtok(NULL, " ");
+    if (!hourStr)
+    {
+        printf("Missing argument\n");
+        return;
+    }
+    int hour = atoi(hourStr);
+
+    const char *minStr = strtok(NULL, " ");
+    if (!minStr)
+    {
+        printf("Missing argument\n");
+        return;
+    }
+    int min = atoi(minStr);
+
+    const char *secStr = strtok(NULL, " ");
+    if (!secStr)
+    {
+        printf("Missing argument\n");
+        return;
+    }
+    int sec = atoi(secStr);
+
+    datetime_t t = {
+        .year = (int16_t)year,
+        .month = (int8_t)month,
+        .day = (int8_t)date,
+        .dotw = 0, // 0 is Sunday
+        .hour = (int8_t)hour,
+        .min = (int8_t)min,
+        .sec = (int8_t)sec};
+    rtc_set_datetime(&t);
+}
+
+static void run_format()
+{
+    const char *arg1 = strtok(NULL, " ");
+    if (!arg1)
+        arg1 = sd_get_by_num(0)->pcName;
+    FATFS *p_fs = sd_get_fs_by_name(arg1);
+    if (!p_fs)
+    {
+        printf("Unknown logical drive number: \"%s\"\n", arg1);
+        return;
+    }
+    /* Format the drive with default parameters */
+    FRESULT fr = f_mkfs(arg1, 0, 0, FF_MAX_SS * 2);
+    if (FR_OK != fr)
+        printf("f_mkfs error: %s (%d)\n", FRESULT_str(fr), fr);
+}
+static void run_mount()
+{
+    const char *arg1 = strtok(NULL, " ");
+    if (!arg1)
+        arg1 = sd_get_by_num(0)->pcName;
+    FATFS *p_fs = sd_get_fs_by_name(arg1);
+    if (!p_fs)
+    {
+        printf("Unknown logical drive number: \"%s\"\n", arg1);
+        return;
+    }
+    FRESULT fr = f_mount(p_fs, arg1, 1);
+    if (FR_OK != fr)
+    {
+        printf("f_mount error: %s (%d)\n", FRESULT_str(fr), fr);
+        return;
+    }
+    sd_card_t *pSD = sd_get_by_name(arg1);
+    myASSERT(pSD);
+    pSD->mounted = true;
+    printf("Processo de montagem do SD ( %s ) concluído\n", pSD->pcName);
+}
+static void run_unmount()
+{
+    const char *arg1 = strtok(NULL, " ");
+    if (!arg1)
+        arg1 = sd_get_by_num(0)->pcName;
+    FATFS *p_fs = sd_get_fs_by_name(arg1);
+    if (!p_fs)
+    {
+        printf("Unknown logical drive number: \"%s\"\n", arg1);
+        return;
+    }
+    FRESULT fr = f_unmount(arg1);
+    if (FR_OK != fr)
+    {
+        printf("f_unmount error: %s (%d)\n", FRESULT_str(fr), fr);
+        return;
+    }
+    sd_card_t *pSD = sd_get_by_name(arg1);
+    myASSERT(pSD);
+    pSD->mounted = false;
+    pSD->m_Status |= STA_NOINIT; // in case medium is removed
+    printf("SD ( %s ) desmontado\n", pSD->pcName);
+}
+static void run_getfree()
+{
+    const char *arg1 = strtok(NULL, " ");
+    if (!arg1)
+        arg1 = sd_get_by_num(0)->pcName;
+    DWORD fre_clust, fre_sect, tot_sect;
+    FATFS *p_fs = sd_get_fs_by_name(arg1);
+    if (!p_fs)
+    {
+        printf("Unknown logical drive number: \"%s\"\n", arg1);
+        return;
+    }
+    FRESULT fr = f_getfree(arg1, &fre_clust, &p_fs);
+    if (FR_OK != fr)
+    {
+        printf("f_getfree error: %s (%d)\n", FRESULT_str(fr), fr);
+        return;
+    }
+    tot_sect = (p_fs->n_fatent - 2) * p_fs->csize;
+    fre_sect = fre_clust * p_fs->csize;
+    printf("%10lu KiB total drive space.\n%10lu KiB available.\n", tot_sect / 2, fre_sect / 2);
+}
+static void run_ls()
+{
+    const char *arg1 = strtok(NULL, " ");
+    if (!arg1)
+        arg1 = "";
+    char cwdbuf[FF_LFN_BUF] = {0};
+    FRESULT fr;
+    char const *p_dir;
+    if (arg1[0])
+    {
+        p_dir = arg1;
+    }
+    else
+    {
+        fr = f_getcwd(cwdbuf, sizeof cwdbuf);
+        if (FR_OK != fr)
+        {
+            printf("f_getcwd error: %s (%d)\n", FRESULT_str(fr), fr);
+            return;
+        }
+        p_dir = cwdbuf;
+    }
+    printf("Directory Listing: %s\n", p_dir);
+    DIR dj;
+    FILINFO fno;
+    memset(&dj, 0, sizeof dj);
+    memset(&fno, 0, sizeof fno);
+    fr = f_findfirst(&dj, &fno, p_dir, "*");
+    if (FR_OK != fr)
+    {
+        printf("f_findfirst error: %s (%d)\n", FRESULT_str(fr), fr);
+        return;
+    }
+    while (fr == FR_OK && fno.fname[0])
+    {
+        const char *pcWritableFile = "writable file",
+                   *pcReadOnlyFile = "read only file",
+                   *pcDirectory = "directory";
+        const char *pcAttrib;
+        if (fno.fattrib & AM_DIR)
+            pcAttrib = pcDirectory;
+        else if (fno.fattrib & AM_RDO)
+            pcAttrib = pcReadOnlyFile;
+        else
+            pcAttrib = pcWritableFile;
+        printf("%s [%s] [size=%llu]\n", fno.fname, pcAttrib, fno.fsize);
+
+        fr = f_findnext(&dj, &fno);
+    }
+    f_closedir(&dj);
+}
+static void run_cat()
+{
+    char *arg1 = strtok(NULL, " ");
+    if (!arg1)
+    {
+        printf("Missing argument\n");
+        return;
+    }
+    FIL fil;
+    FRESULT fr = f_open(&fil, arg1, FA_READ);
+    if (FR_OK != fr)
+    {
+        printf("f_open error: %s (%d)\n", FRESULT_str(fr), fr);
+        return;
+    }
+    char buf[256];
+    while (f_gets(buf, sizeof buf, &fil))
+    {
+        printf("%s", buf);
+    }
+    fr = f_close(&fil);
+    if (FR_OK != fr)
+        printf("f_open error: %s (%d)\n", FRESULT_str(fr), fr);
+}
+
+// Função para capturar dados do ADC e salvar no arquivo *.txt
+void capture_adc_data_and_save()
+{
+    printf("\nCapturando dados do ADC. Aguarde finalização...\n");
+    FIL file;
+    FRESULT res = f_open(&file, filename, FA_WRITE | FA_CREATE_ALWAYS);
+    if (res != FR_OK)
+    {
+        printf("\n[ERRO] Não foi possível abrir o arquivo para escrita. Monte o Cartao.\n");
+        return;
+    }
+    for (int i = 0; i < 128; i++)
+    {
+        adc_select_input(0);
+        uint16_t adc_value = adc_read();
+        char buffer[50];
+        sprintf(buffer, "%d %d\n", i + 1, adc_value);
+        UINT bw;
+        res = f_write(&file, buffer, strlen(buffer), &bw);
+        if (res != FR_OK)
+        {
+            printf("[ERRO] Não foi possível escrever no arquivo. Monte o Cartao.\n");
+            f_close(&file);
+            return;
+        }
+        sleep_ms(100);
+    }
+    f_close(&file);
+    printf("\nDados do ADC salvos no arquivo %s.\n\n", filename);
+}
+
+// Função para ler o conteúdo de um arquivo e exibir no terminal
+void read_file(const char *filename)
+{
+    FIL file;
+    FRESULT res = f_open(&file, filename, FA_READ);
+    if (res != FR_OK)
+    {
+        printf("[ERRO] Não foi possível abrir o arquivo para leitura. Verifique se o Cartão está montado ou se o arquivo existe.\n");
+
+        return;
+    }
+    char buffer[128];
+    UINT br;
+    printf("Conteúdo do arquivo %s:\n", filename);
+    while (f_read(&file, buffer, sizeof(buffer) - 1, &br) == FR_OK && br > 0)
+    {
+        buffer[br] = '\0';
+        printf("%s", buffer);
+    }
+    f_close(&file);
+    printf("\nLeitura do arquivo %s concluída.\n\n", filename);
+}
+
+// Trecho para modo BOOTSEL com botão B
+#include "pico/bootrom.h"
+#define botaoB 6
+void gpio_irq_handler(uint gpio, uint32_t events)
+{
+    reset_usb_boot(0, 0);
+}
+
+static void run_help()
+{
+    printf("\nComandos disponíveis:\n\n");
+    printf("Digite 'a' para montar o cartão SD\n");
+    printf("Digite 'b' para desmontar o cartão SD\n");
+    printf("Digite 'c' para listar arquivos\n");
+    printf("Digite 'd' para mostrar conteúdo do arquivo\n");
+    printf("Digite 'e' para obter espaço livre no cartão SD\n");
+    printf("Digite 'f' para capturar dados do ADC e salvar no arquivo\n");
+    printf("Digite 'g' para formatar o cartão SD\n");
+    printf("Digite 'h' para exibir os comandos disponíveis\n");
+    printf("\nEscolha o comando:  ");
+}
+
+typedef void (*p_fn_t)();
+typedef struct
+{
+    char const *const command;
+    p_fn_t const function;
+    char const *const help;
+} cmd_def_t;
+
+static cmd_def_t cmds[] = {
+    {"setrtc", run_setrtc, "setrtc <DD> <MM> <YY> <hh> <mm> <ss>: Set Real Time Clock"},
+    {"format", run_format, "format [<drive#:>]: Formata o cartão SD"},
+    {"mount", run_mount, "mount [<drive#:>]: Monta o cartão SD"},
+    {"unmount", run_unmount, "unmount <drive#:>: Desmonta o cartão SD"},
+    {"getfree", run_getfree, "getfree [<drive#:>]: Espaço livre"},
+    {"ls", run_ls, "ls: Lista arquivos"},
+    {"cat", run_cat, "cat <filename>: Mostra conteúdo do arquivo"},
+    {"help", run_help, "help: Mostra comandos disponíveis"}};
+
+static void process_stdio(int cRxedChar)
+{
+    static char cmd[256];
+    static size_t ix;
+
+    if (!isprint(cRxedChar) && !isspace(cRxedChar) && '\r' != cRxedChar &&
+        '\b' != cRxedChar && cRxedChar != (char)127)
+        return;
+    printf("%c", cRxedChar); // echo
+    stdio_flush();
+    if (cRxedChar == '\r')
+    {
+        printf("%c", '\n');
+        stdio_flush();
+
+        if (!strnlen(cmd, sizeof cmd))
+        {
+            printf("> ");
+            stdio_flush();
+            return;
+        }
+        char *cmdn = strtok(cmd, " ");
+        if (cmdn)
+        {
+            size_t i;
+            for (i = 0; i < count_of(cmds); ++i)
+            {
+                if (0 == strcmp(cmds[i].command, cmdn))
+                {
+                    (*cmds[i].function)();
+                    break;
+                }
+            }
+            if (count_of(cmds) == i)
+                printf("Command \"%s\" not found\n", cmdn);
+        }
+        ix = 0;
+        memset(cmd, 0, sizeof cmd);
+        printf("\n> ");
+        stdio_flush();
+    }
+    else
+    {
+        if (cRxedChar == '\b' || cRxedChar == (char)127)
+        {
+            if (ix > 0)
+            {
+                ix--;
+                cmd[ix] = '\0';
+            }
+        }
+        else
+        {
+            if (ix < sizeof cmd - 1)
+            {
+                cmd[ix] = cRxedChar;
+                ix++;
+            }
+        }
+    }
+}
 
 int main()
 {
-    stdio_init_all();
+    // Para ser utilizado o modo BOOTSEL com botão B
+    gpio_init(botaoB);
+    gpio_set_dir(botaoB, GPIO_IN);
+    gpio_pull_up(botaoB);
+    gpio_set_irq_enabled_with_callback(botaoB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    stdio_init_all();
+    sleep_ms(5000);
+    time_init();
+    adc_init();
+
+    printf("FatFS SPI example\n");
+    printf("\033[2J\033[H"); // Limpa tela
+    printf("\n> ");
+    stdio_flush();
+    //    printf("A tela foi limpa...\n");
+    //    printf("Depois do Flush\n");
+    run_help();
+    while (true)
+    {
+        int cRxedChar = getchar_timeout_us(0);
+        if (PICO_ERROR_TIMEOUT != cRxedChar)
+            process_stdio(cRxedChar);
+
+        if (cRxedChar == 'a') // Monta o SD card se pressionar 'a'
+        {
+            printf("\nMontando o SD...\n");
+            run_mount();
+            printf("\nEscolha o comando (h = help):  ");
+        }
+        if (cRxedChar == 'b') // Desmonta o SD card se pressionar 'b'
+        {
+            printf("\nDesmontando o SD. Aguarde...\n");
+            run_unmount();
+            printf("\nEscolha o comando (h = help):  ");
+        }
+        if (cRxedChar == 'c') // Lista diretórios e os arquivos se pressionar 'c'
+        {
+            printf("\nListagem de arquivos no cartão SD.\n");
+            run_ls();
+            printf("\nListagem concluída.\n");
+            printf("\nEscolha o comando (h = help):  ");
+        }
+        if (cRxedChar == 'd') // Exibe o conteúdo do arquivo se pressionar 'd'
+        {
+            read_file(filename);
+            printf("Escolha o comando (h = help):  ");
+        }
+        if (cRxedChar == 'e') // Obtém o espaço livre no SD card se pressionar 'e'
+        {
+            printf("\nObtendo espaço livre no SD.\n\n");
+            run_getfree();
+            printf("\nEspaço livre obtido.\n");
+            printf("\nEscolha o comando (h = help):  ");
+        }
+        if (cRxedChar == 'f') // Captura dados do ADC e salva no arquivo se pressionar 'f'
+        {
+            capture_adc_data_and_save();
+            printf("\nEscolha o comando (h = help):  ");
+        }
+        if (cRxedChar == 'g') // Formata o SD card se pressionar 'g'
+        {
+            printf("\nProcesso de formatação do SD iniciado. Aguarde...\n");
+            run_format();
+            printf("\nFormatação concluída.\n\n");
+            printf("\nEscolha o comando (h = help):  ");
+        }
+        if (cRxedChar == 'h') // Exibe os comandos disponíveis se pressionar 'h'
+        {
+            run_help();
+        }
+        sleep_ms(500);
     }
+    return 0;
 }
